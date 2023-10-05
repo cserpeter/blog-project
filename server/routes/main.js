d8e65584aecd8a2328d51478cfcb64079264ae77
Cseresnyés Péter <csefid@gmail.com>
2023-10-05 08:15:48 +0200

    search post function added

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,14 +1,94 @@
 const express = require('express');
 const router = express.Router();
 
-//Routes
-router.get('', (req, res) => {
-  const locals = {
-    title: 'NodeJs Blog',
-    description: 'Simple blog created with NodeJs, Express and MongoDB',
-  };
+const Post = require('../models/Post');
 
-  res.render('index', { locals });
+//Routes
+router.get('', async (req, res) => {
+  try {
+    const locals = {
+      title: 'NodeJs Blog',
+      description: 'Simple blog created with NodeJs, Express and MongoDB',
+    };
+
+    let perPage = 10;
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+    const count = await Post.count();
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render('index', {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/* function insertPostData() {
+  Post.insertMany([
+    {
+      title: 'Building a Blog',
+      body: 'This is the body text',
+    },
+    {
+      title: 'Building another Blog',
+      body: 'This is yet again the body text',
+    },
+  ]);
+}
+insertPostData(); */
+
+router.get('/post/:id', async (req, res) => {
+  try {
+    let slug = req.params.id;
+
+    const data = await Post.findById({ _id: slug });
+
+    const locals = {
+      title: data.title,
+      description: 'Simple blog created with NodeJs, Express and MongoDB',
+    };
+
+    res.render('post', {
+      locals,
+      data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post('/search', async (req, res) => {
+  try {
+    const locals = {
+      title: 'Search',
+      description: 'Simple blog created with NodeJs, Express and MongoDB',
+    };
+
+    let searchTerm = req.body.searchTerm;
+    const searchNoSpecialChar = searchTerm.replace(/^[a-zA-Z0-9]/g, '');
+
+    const data = await Post.find({
+      $or: [
+        { title: { $regex: new RegExp(searchNoSpecialChar, 'i') } },
+        { body: { $regex: new RegExp(searchNoSpecialChar, 'i') } },
+      ],
+    });
+
+    res.render('search', { data, locals });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.get('/about', (req, res) => {
